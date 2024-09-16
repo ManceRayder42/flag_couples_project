37b07bbb6b6b614b27e7422f26f4c84421711591
Or <orgelb06@gmail.com>
2024-09-16 10:37:55 +0300

    consts

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello there')
+import pygame
+import screen
+import consts
+
+
+
+while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
