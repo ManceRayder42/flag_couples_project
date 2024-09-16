79fdb5fb7dfd99a5e18bfbd4a0b41764bb225d34
Or <orgelb06@gmail.com>
2024-09-16 10:55:06 +0300

    pull

---
+++ consts.py
@@ -0,0 +1,8 @@
+import pygame
+
+EXPLOSION = pygame.image.load("explotion.png")
+FLAG = pygame.image.load("flag.png")
+GRASS = pygame.image.load("grass.png")
+MINE = pygame.image.load("mine.png")
+SOLDIER = pygame.image.load("soldier.png")
+COLOR = (0, 255, 0)
